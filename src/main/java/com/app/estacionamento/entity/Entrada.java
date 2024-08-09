0f1c1577d82fcfb4e87eab5c5ff46ce398b77dc7
felipreis <felipreisdaniel@gmail.com>
2024-08-09 14:27:21 -0300

    mapeando db entrada

--- src/main/java/com/app/estacionamento/entity/Entrada.java
+++ src/main/java/com/app/estacionamento/entity/Entrada.java
@@ -1,12 +1,27 @@
 package com.app.estacionamento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_entrada")
 public class Entrada {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "carro_id")
     private Carro carro;
 
     private LocalDate dataEntrada = LocalDate.now();
