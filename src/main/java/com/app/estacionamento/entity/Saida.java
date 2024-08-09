6d8f27deb9e696f8753e828a42643580b10b3807
felipreis <felipreisdaniel@gmail.com>
2024-08-09 14:27:47 -0300

    mapeando db saida

--- src/main/java/com/app/estacionamento/entity/Saida.java
+++ src/main/java/com/app/estacionamento/entity/Saida.java
@@ -1,11 +1,25 @@
 package com.app.estacionamento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_saida")
 public class Saida {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "entrada_id")
     private Entrada entrada;
 
     private LocalDateTime horaSaida = LocalDateTime.now();
