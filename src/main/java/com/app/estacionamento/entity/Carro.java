cee80c191b5b9550ce95c6d5e231cbfdcdea046b
felipreis <felipreisdaniel@gmail.com>
2024-08-09 14:26:51 -0300

    mapeando db carro

--- src/main/java/com/app/estacionamento/entity/Carro.java
+++ src/main/java/com/app/estacionamento/entity/Carro.java
@@ -1,11 +1,34 @@
 package com.app.estacionamento.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_carro")
+@JsonIgnoreProperties({"hibernanteLazyInitializer","handler"})
 public class Carro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String marca;
 
     private String placa;
+
+    @OneToMany(mappedBy = "carro", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Entrada> entradas = new ArrayList<>();
+
 
 }
