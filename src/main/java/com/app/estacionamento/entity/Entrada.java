4304642e7c8386b9088bdb4682716fdfe008bc27
felipreis <felipreisdaniel@gmail.com>
2024-08-10 18:38:28 -0300

    integração tipo de dado id

--- src/main/java/com/app/estacionamento/entity/Entrada.java
+++ src/main/java/com/app/estacionamento/entity/Entrada.java
@@ -18,7 +18,7 @@ public class Entrada {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "carro_id")
