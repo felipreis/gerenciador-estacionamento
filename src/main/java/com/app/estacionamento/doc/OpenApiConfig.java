10ccc3ee692c101d8adb8634fbfd42d679f0eaa6
felipreis <felipreisdaniel@gmail.com>
2024-08-12 20:21:23 -0300

    configuração SpringDoc

--- src/main/java/com/app/estacionamento/doc/OpenApiConfig.java
+++ src/main/java/com/app/estacionamento/doc/OpenApiConfig.java
@@ -1,9 +1,20 @@
 package com.app.estacionamento.doc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Meu Projeto de Estacionamento",
+                description = "API REST para gerenciamento de estacionamento",
+                version = "1.0.0"
+        )
+)
 public class OpenApiConfig {
 
     @Bean
