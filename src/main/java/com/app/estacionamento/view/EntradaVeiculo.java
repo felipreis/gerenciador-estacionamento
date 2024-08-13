93a9e50116522ce98bb4e0182da24a7085ac8d73
felipreis <felipreisdaniel@gmail.com>
2024-08-13 20:39:15 -0300

    teste tela entrada

---
+++ src/main/java/com/app/estacionamento/view/EntradaVeiculo.java
@@ -0,0 +1,9 @@
+package com.app.estacionamento.view;
+
+import javax.swing.*;
+
+public class EntradaVeiculo {
+    private JTextField marca;
+    private JButton entradaButton;
+    private JTextField placa;
+}
