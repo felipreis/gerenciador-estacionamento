cc323382ac101f59c162eba9a191d5127441a80d
felipreis <felipreisdaniel@gmail.com>
2024-08-14 14:47:10 -0300

    removendo package view

--- src/main/java/com/app/estacionamento/GerenciadorEstacionamentoApplication.java
+++ src/main/java/com/app/estacionamento/GerenciadorEstacionamentoApplication.java
@@ -1,11 +1,7 @@
 package com.app.estacionamento;
 
-import com.app.estacionamento.view.EntradaVeiculo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.swing.*;
 
 @SpringBootApplication
 public class GerenciadorEstacionamentoApplication {
