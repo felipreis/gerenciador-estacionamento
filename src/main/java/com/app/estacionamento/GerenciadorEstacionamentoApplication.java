19eaf06cbcf374ea463a116ead60253d362a7571
felipreis <felipreisdaniel@gmail.com>
2024-08-13 22:45:29 -0300

    erro nao solucionado no registro da entrada

--- src/main/java/com/app/estacionamento/GerenciadorEstacionamentoApplication.java
+++ src/main/java/com/app/estacionamento/GerenciadorEstacionamentoApplication.java
@@ -1,13 +1,19 @@
 package com.app.estacionamento;
 
+import com.app.estacionamento.view.EntradaVeiculo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class GerenciadorEstacionamentoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorEstacionamentoApplication.class, args);
+		 SpringApplication.run(GerenciadorEstacionamentoApplication.class, args);
 	}
+
+
 
 }
--- src/main/java/com/app/estacionamento/view/EntradaVeiculo.java
+++ src/main/java/com/app/estacionamento/view/EntradaVeiculo.java
@@ -5,7 +5,6 @@ import com.app.estacionamento.entity.Carro;
 import com.app.estacionamento.entity.Entrada;
 import com.app.estacionamento.entity.form.CarroForm;
 import com.app.estacionamento.entity.form.EntradaForm;
-import com.app.estacionamento.repository.CarroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.swing.*;
@@ -13,38 +12,73 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import com.app.estacionamento.controller.CarrroController;
+import org.springframework.stereotype.Component;
 
-public class EntradaVeiculo {
+@Component
+public class EntradaVeiculo extends JFrame{
 
     private JTextField marca;
-    private JButton entradaButton;
     private JTextField placa;
+    private JButton entradaButton;
 
-    public EntradaVeiculo() {
+    private final CarrroController carroController;
+    private final EntradaController entradaController;
+
+    @Autowired
+    public EntradaVeiculo(CarrroController carroController, EntradaController entradaController) {
+        this.carroController = carroController;
+        this.entradaController = entradaController;
+
+        // Configuração dos componentes da interface gráfica
+        initUI();
+    }
+
+    private void initUI() {
+        marca = new JTextField(20);
+        placa = new JTextField(20);
+        entradaButton = new JButton("Entrada");
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        add(new JLabel("Marca:"));
+        add(marca);
+        add(new JLabel("Placa:"));
+        add(placa);
+        add(entradaButton);
+
         entradaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                CarroForm carroForm = new CarroForm();
-                Carro carro = new Carro();
-                CarrroController carroController =  new CarrroController();
-                EntradaForm entradaForm = new EntradaForm();
-                EntradaController entradaController = new EntradaController();
-
-                carroForm.setMarca(marca.getText());
-                carroForm.setPlaca(placa.getText());
-
-                carro = carroController.create(carroForm);
-
-                entradaForm.setCarroId(carro.getId());
-
-                entradaController.create(entradaForm);
-
-                System.out.println("entrada do veiculo realizada");
-
-
-
+                handleEntrada();
             }
         });
+
+        setTitle("Entrada de Veículo");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
     }
+
+    private void handleEntrada() {
+        CarroForm carroForm = new CarroForm();
+        EntradaForm entradaForm = new EntradaForm();
+
+        carroForm.setMarca(marca.getText());
+        carroForm.setPlaca(placa.getText());
+
+        Carro carro = carroController.create(carroForm);
+
+
+        entradaForm.setCarroId(carro.getId());
+
+        Entrada entrada = entradaController.create(entradaForm);
+
+        System.out.println("Entrada do veículo realizada");
+    }
+
+    // Método para inicializar e mostrar a janela
+    public void showWindow() {
+        SwingUtilities.invokeLater(() -> setVisible(true));
+    }
+
+
 }
